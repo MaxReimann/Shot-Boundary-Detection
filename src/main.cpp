b5190423a243938da12e6844e631a751b8ed45ee
Stefan Bunk <stefan.bunk@googlemail.com>
2015-05-04 10:45:15 +0200

    Add sample image and change executable name to sbd.

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,9 @@ using namespace cv;
 int main(int argc, char** argv) {
     Histogram hist;
     hist.buildHistogram();
-}
-
-
-int openCvSample(int argc, char** argv) {
-    if (argc != 2) {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
 
     Mat image;
-    image = imread(argv[1], 1);
+    image = imread("../resources/cat.jpg", 1);
 
     if (!image.data) {
         printf("No image data \n");
