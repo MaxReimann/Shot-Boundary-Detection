a25f344d3025e2a9eeab6ea8fca6725234c5b19e
Tanja Bergmann <tabergma@gmail.com>
2015-05-26 15:06:31 +0200

    Remove unnecessary methods in histbuilder.

--- src/histogram/histogram.hpp
+++ src/histogram/histogram.hpp
@@ -11,8 +11,6 @@ namespace sbd {
         int getHistSize();
         cv::Mat buildHistogram(const cv::Mat& image);
         cv::Mat buildHistogram1Channel(const cv::Mat& image);
-        static void displayHistogram(const cv::Mat& hist);
-        static void plotRGBHistogram(const cv::Mat &srcImage, int width, int height);
         cv::Mat convertMat(const cv::Mat& hist);
         cv::Mat convertMat1Channel(const cv::Mat& hist);
         std::vector<float> getAbsChanges(const cv::Mat& diffs);
