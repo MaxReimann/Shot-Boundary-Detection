bc2ede6521aee9802154c9be3fc21548e9221b55
Tanja Bergmann <tabergma@gmail.com>
2015-06-03 11:56:34 +0200

    Add net.

---
+++ nets/make_some_nice_plots.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+TRAINING_LOG="uc101.tlog"
+
+if [ `whoami` == "mpss2015" ]; then
+	echo "Please run from a local user."
+	exit 1
+else
+#	scp mpss2015@172.16.23.193:~/video-classification/nets/activity_recognition/$TRAINING_LOG . &> /dev/null
+	scp mpss2015@172.16.23.193:~/caffe-tmbo/examples/activity_recognition/$TRAINING_LOG . &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "Could not download training log file '$TRAINING_LOG'"
+		exit 2
+	fi
+	$CAFFE_ROOT/tools/extra/parse_log.sh $TRAINING_LOG
+	gnuplot -p plot_log.gnuplot
+fi
