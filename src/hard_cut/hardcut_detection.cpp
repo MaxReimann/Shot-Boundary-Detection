8a899b5fb64b2655f2491b353cae215a367f2964
SBD team <stefan.bunk@student.hpi.uni-potsdam.de>
2015-08-16 14:34:03 +0200

    remove necessary type in classify folder

--- src/hard_cut/hardcut_detection.cpp
+++ src/hard_cut/hardcut_detection.cpp
@@ -19,7 +19,7 @@ using namespace sbd;
 
 
 int HardCutMain::main(po::variables_map flagArgs, std::map<std::string, std::string> inputArguments) {
-    bool USE_CACHED_HISTOGRAMS = false;
+    bool USE_CACHED_HISTOGRAMS = true;
 
     std::string dataFolder = inputArguments.at("data_folder");
 
@@ -67,15 +67,15 @@ int HardCutMain::main(po::variables_map flagArgs, std::map<std::string, std::str
 		std::string histogramCachePath = "../resources/differenceHistogramsEvaluation.yaml";
 		cv::FileStorage fs2;
 
-		if (!USE_CACHED_HISTOGRAMS || !boost::filesystem::exists(histogramCachePath))
+		if (true || !USE_CACHED_HISTOGRAMS || !boost::filesystem::exists(histogramCachePath))
 		{
 
-            std::vector<std::string> imagePathsTest = getFileNames(classifyPath);
+            std::vector<std::string> imagePathsTest = getFileNames(classifyPath,false);
             visImagePaths = imagePathsTest;
 			testSet = buildHistogramDifferences(imagePathsTest, gold);
 
 
-			if (USE_CACHED_HISTOGRAMS)
+			if (false && USE_CACHED_HISTOGRAMS)
 			{
 				std::cout << "Caching built histograms." << std::endl;
 				fs2.open(histogramCachePath, cv::FileStorage::WRITE);
@@ -106,7 +106,7 @@ int HardCutMain::main(po::variables_map flagArgs, std::map<std::string, std::str
     visDiffs = testSet.values;
 
     // the needed visualization data can just be createded if a classify_folder was used und we did not use the cache
-    if (!USE_CACHED_HISTOGRAMS && flagArgs.count("classify_folder")) {
+    if (flagArgs.count("classify_folder")) {
         writeVisualizationData(visImagePaths, Histogram::getAbsChanges(visDiffs), visGolds, visPredictions);
     }
 
@@ -140,12 +140,12 @@ std::unordered_set<sbd::GoldStandardElement> HardCutMain::readGoldStandard(std::
  * 2.
  * Read the frame file names recursively.
  */
-std::vector<std::string> HardCutMain::getFileNames(std::string dataFolder) {
+std::vector<std::string> HardCutMain::getFileNames(std::string dataFolder, bool folderReplacement) {
     printf("Getting frame file names.\n");
 
     std::vector<boost::filesystem::path> imagePaths;
     std::string extension = ".jpg";
-    if (dataFolder.find("[type]") == std::string::npos)
+    if (dataFolder.find("[type]") == std::string::npos && folderReplacement)
         wrongUsageHardCut();
     std::string framesFolder = boost::replace_first_copy(dataFolder, "[type]", "frames");
     std::cout << "Reading frames from " << framesFolder << std::endl;
